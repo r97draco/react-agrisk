ae387e1e5953baa675eaf234a352b95862699423
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-06-03 18:37:11 -0600

    clean up

--- src/Pages/DataAccess/Download.js
+++ src/Pages/DataAccess/Download.js
@@ -8,18 +8,23 @@ import axios from "axios";
 import { Alert } from "@mui/material";
 import { saveAs } from "file-saver";
 
-const fileLink= "https://5u8lxhfkbj.execute-api.ca-central-1.amazonaws.com/download-era5-gars-data?username=rommelnuque05&filename=test_ericknuque_20230601000000.csv";
+
+// const jun3link = "https://h57y6aazo9.execute-api.ca-central-1.amazonaws.com/download-era5-gars-data?username=rommelnuque05&filename=test_ericknuque_20230601000000.csv";
+
+
+const photoURL = "https://picsum.photos/200";
+
 const Download = () => {
   const [file, setFile] = useState(null);
   const [fileInput, setFileInput] = useState(
     "test_ericknuque_20230601000000.csv"
   );
+  const [urlInput, setUrlInput] = useState("https://picsum.photos/200");
   const [isDownloaded, setIsDownloaded] = useState("undefined");
 
   const handleDownload = async () => {
-
-
-    const endpoint ="https://5u8lxhfkbj.execute-api.ca-central-1.amazonaws.com/download-era5-gars-data";
+    //-------------------------------------------------------------
+    const endpoint = "https://h57y6aazo9.execute-api.ca-central-1.amazonaws.com/download-era5-gars-data";
     const username = "rommelnuque05";
     const filename = fileInput !== "" ? fileInput : "test_ericknuque_20230601000000.csv";
     const params = {
@@ -27,30 +32,39 @@ const Download = () => {
       filename: filename,
     };
     const queryString = new URLSearchParams(params).toString();
-    const url = `${endpoint}?${queryString}`;
-    console.log("url: ", url);
+    const URL = `${endpoint}?${queryString}`;
+    //-------------------------------------------------------------
 
-    const downloadFile =async (url, filename) => {
-      fetch(url, {
+    try {
+      const response = await axios({
+        url: URL,
         method: "GET",
-        mode: 'no-cors',
-      })
-        .then((response) => {
-          console.log(response.data);
-          console.log(response.status);
-          console.log(response.statusText);
-          console.log(response.headers);
-          console.log(response.config);
-          const blob = new Blob([response.data], { type: response.headers['content-type'] });
-          saveAs(blob, filename); // Save the blob as a file using the provided filename
-          setIsDownloaded("Successful");
-        })
-        .catch((error) => {
-          console.error('Error in file download:', error);
-          setIsDownloaded("Unsuccessful");
-        });
-    };
-    downloadFile(url, fileInput);
+        responseType: "blob",
+      });      
+      console.log(`Res`, response);
+      let filename = "file";
+      const contentDisposition = response.headers["content-disposition"];
+      if (contentDisposition) {
+        const filenameMatch = contentDisposition.match(/filename=(.+)$/i);
+        if (filenameMatch) {
+          filename = filenameMatch[1];
+        }
+      } else {
+        // const urlSegments = url.split("/");
+        // filename = urlSegments[urlSegments.length - 1];
+        filename= fileInput !==""?fileInput: "file";
+        // filename="file"
+      }
+      if (response.status ==200) {
+        const blob = new Blob([response.data], {type: response.headers["content-type"],});
+        saveAs(blob, filename);
+      } else {
+        console.error("Response is not OK:", response.status);
+        throw new Error("Response is not OK");
+      }
+    } catch (error) {
+      console.error("Error in file download:", error);
+    }
   };
 
   return (
@@ -67,7 +81,7 @@ const Download = () => {
           margin="normal"
           required
           sx={{ width: { xs: "auto", sm: 400 } }}
-          name="password"
+          name="filename"
           label="Search a file to download…"
           type="text"
           defaultValue={fileInput}
@@ -94,7 +108,7 @@ const Download = () => {
             component="label"
             sx={{ width: 200 }}
           >
-            Download            
+            Download
           </Button>
         )}
       </Stack>
@@ -107,6 +121,66 @@ const Download = () => {
     </div>
   );
 };
+export default Download;
+
+// const handleDownload = async ({ fileInput, setIsDownloaded }) => {
+//   const endpoint =
+//     "https://5u8lxhfkbj.execute-api.ca-central-1.amazonaws.com/download-era5-gars-data";
+//   const username = "rommelnuque05";
+//   const filename =
+//     fileInput !== "" ? fileInput : "test_ericknuque_20230601000000.csv";
+//   const params = {
+//     username: username,
+//     filename: filename,
+//   };
+//   const queryString = new URLSearchParams(params).toString();
+//   const url = `${endpoint}?${queryString}`;
+//   // console.log("url: ", url);
+//   const testUrl =
+//     "https://gist.githubusercontent.com/taterbase/2784890/raw/ee2f562a0dce92e922756b8411d4e1c0bcddb159/bin2string.js";
+//   const downloadFile = async (url, filename) => {
+//     await fetch(url, {
+//       method: "GET",
+//       mode: "no-cors",
+//     })
+//       .then((response) => {
+//         console.table(response);
+//         console.log(response);
+//         console.log(response.config);
+//         const blob = new Blob([response.data], {
+//           type: response.headers["content-type"],
+//         });
+//         saveAs(blob, filename); // Save the blob as a file using the provided filename
+//         setIsDownloaded("Successful");
+//       })
+//       .catch((error) => {
+//         console.error("Error in file download:", error);
+//         setIsDownloaded("Unsuccessful");
+//       });
+//   };
+//   const gistUrl =
+//     "https://api.github.com/gists/53e1780a5a68fe9281cfbbc9820d381f";
+//   const gist = "https://gist.github.com/schacon/1";
+
+//   const downloadGist = (url) => {
+//     fetch(url)
+//       .then((results) => {
+//         console.log(results);
+//         return results.json();
+//       })
+//       .then((data) => {
+//         console.log(data.files["forgeLikeServerSetup.md"].content);
+//         // console.log(data.files[filename].content);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   };
+//   // downloadFile(testUrl, fileInput);
+//   // downloadGist(testUrl);
+//   // downloadGist(gist);
+//   downloadGist(gistUrl);
+// };
 
 // const Download = () => {
 //   const [file, setFile] = useState(false);
@@ -187,4 +261,3 @@ const Download = () => {
 //   );
 // };
 
-export default Download;
