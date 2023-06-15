1641344ca3b1a840582a166e78ce4334473442e7
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-06-15 15:42:08 -0600

    fixed download

--- src/Pages/DataAccess/Download.js
+++ src/Pages/DataAccess/Download.js
@@ -11,6 +11,8 @@ import TextField from "@mui/material/TextField";
 import axios from "axios";
 import { Alert } from "@mui/material";
 import { saveAs } from "file-saver";
+import { FileCopyOutlined } from "@material-ui/icons";
+import { Link } from "react-router-dom";
 
 /**
  * DownloadComponent renders a text input component to input a filename from the user and enables downloading the file from an API.
@@ -20,6 +22,9 @@ import { saveAs } from "file-saver";
 const Download = () => {
   const [file, setFile] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [downLoading, setDownLoading] = useState(false);
+  const [preSignedUrl, setPreSignedUrl] = useState();
+  const [selected, setSelected] = useState();
   const [fileList, setFileList] = useState({
     status: 0,
     count: 0,
@@ -30,6 +35,7 @@ const Download = () => {
   const [isDownloaded, setIsDownloaded] = useState("undefined");
 
   const searchFile = async () => {
+    setLoading(true);
     const URL =
       "https://hmtrekg8w0.execute-api.ca-central-1.amazonaws.com/search-era5-gars-data";
     try {
@@ -43,25 +49,42 @@ const Download = () => {
       console.log("Res from search", response);
       if (response.data["filelist"].length > 0) setFile(true);
       if (response.status === 200) {
-        setFileList(response.data)
+        setFileList(response.data);
         // fileList.status = response.data["status"];
         // fileList.count = response.data["count"];
         // fileList.filelist = response.data["filelist"];
-        setFilteredList(response.data["filelist"])
+        setFilteredList(response.data["filelist"]);
       }
     } catch (error) {
       console.error("Error in file Search:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
   /**
    * handleDownload function is responsible for initiating the file download process from an API asynchronously.
    */
-  const handleDownload = async () => {
-    //----------------GENERATING THE URL------------------------------------
+  // const setDownloadLink = (selectedItem) => {
+  //   if (selectedItem === selected) {
+  //     setSelected(null);
+  //     setPresigedURL("");
+  //   } else {
+  //     setSelected(selectedItem);
+  //     setPresigedURL(selectedItem); // Replace this with your logic to get the presigned URL
+  //   }
+  // };
+  const handleDownload = async (selectedItem) => {
+    if (selectedItem === selected) {
+      setSelected(null);
+      setPreSignedUrl();
+    } else {
+    setSelected(selectedItem);
+    
+    // setPreSignedUrl();
+    setDownLoading(true);
     const URL =
       "https://hmtrekg8w0.execute-api.ca-central-1.amazonaws.com/download-era5-gars-data";
-    setLoading(true);
 
     try {
       const response = await axios({
@@ -69,23 +92,17 @@ const Download = () => {
         method: "GET",
         params: {
           username: "rommelnuque",
-          filename: downloadFile,
+          filename: selectedItem,
         },
       });
-      console.log(downloadFile)
+      console.log(downloadFile);
       console.log(`Res`, response);
       // console.log(`Content Length: `, response.data["size"]);
 
-      if (
-        response.status === 200 &&
-        response.data &&
-        response.data["size"] > 0
-      ) {
-        const blob = new Blob([response.data], {
-          type: response.headers["content-type"],
-        });
-        // saveAs(blob, filename);
-        setIsDownloaded("Successful");
+      if (response.status === 200) {
+        let signedUrl = response.data["presigned-url"];
+        setPreSignedUrl(signedUrl);
+        // setIsDownloaded("Successful");
       } else {
         console.error(
           "Response is not OK || No file present:",
@@ -98,15 +115,18 @@ const Download = () => {
       setIsDownloaded("Unsuccessful");
       console.error("Error in file download:", error);
     } finally {
-      setLoading(false);
+      setDownLoading(false);
     }
+  }
   };
-  const [filteredList, setFilteredList]= useState(fileList["filelist"])
+  const [filteredList, setFilteredList] = useState(fileList["filelist"]);
   const filterList = (text) => {
     if (text !== "") {
-      setFilteredList(fileList["filelist"].filter((item) => {
-        return item.toLowerCase().includes(text.toLowerCase());
-      }))
+      setFilteredList(
+        fileList["filelist"].filter((item) => {
+          return item.toLowerCase().includes(text.toLowerCase());
+        })
+      );
     }
     console.log(fileList["filelist"]);
   };
@@ -137,12 +157,13 @@ const Download = () => {
         <Button
           variant="contained"
           component="label"
-          sx={{ width: 200, flexGrow: 1 }}
+          sx={{ width: 200 }}
           onClick={searchFile}
+          disabled={loading}
         >
-          Search File
+          {loading ? <CircularProgress size={27} /> : "Search File"}
         </Button>
-        <Button
+        {/* <Button
           variant="contained"
           onClick={handleDownload}
           component="label"
@@ -150,6 +171,18 @@ const Download = () => {
           disabled={loading}
         >
           {loading ? <CircularProgress size={27} /> : "Download"}
+        </Button> */}
+        <Button
+          component={Link}
+          to={preSignedUrl}
+          disabled={!preSignedUrl}
+          variant="contained"
+          sx={{ width: 200 }}
+          target="_blank"
+          download={true}
+          onClick={() => setIsDownloaded("Successful")}
+        >
+          {downLoading ? <CircularProgress size={27} /> : "Download"}
         </Button>
       </Stack>
       {isDownloaded === "Successful" && (
@@ -163,17 +196,39 @@ const Download = () => {
           Total Results : {filteredList.length}
         </Typography>
       )}
+      {/* {file &&
+        filteredList.map((item, index) => (
+          <FormControlLabel
+            key={index}
+            value={item}
+            className="w-full p-2"
+            control={<Checkbox onChange={() => handleDownload(item)} />}
+            label={
+              <span>
+                {item} <FileCopyOutlined />
+              </span>
+            }
+          />
+        ))}
+      ----------------------------------------------------- */}
       {file &&
         filteredList.map((item, index) => (
-          <>
-            <FormControlLabel
-              key={index}
-              value={item}
-              className="w-full p-2"
-              control={<Checkbox onChange={(item) => setDowloadFile(item.toString())} />}
-              label={item}
-            />
-          </>
+          <FormControlLabel
+            key={index}
+            value={item}
+            className="w-full p-2"
+            control={
+              <Checkbox
+                checked={selected === item}
+                onChange={() => handleDownload(item)}
+              />
+            }
+            label={
+              <span>
+                {item} <FileCopyOutlined />
+              </span>
+            }
+          />
         ))}
     </div>
   );
